7e85246044f9227990820c57c617b2b1709b0bf4
DiegoBracci <dbraccipro@gmail.com>
2020-12-18 19:12:34 +0000

    Final Commit

--- src/main.js
+++ src/main.js
@@ -6,16 +6,28 @@ import "./style.css";
 window.onload = function() {
   document.querySelector("#btn").addEventListener("click", () => {
     let result = generateSimbol();
-    document.querySelector("#simbol1").innerHTML = result;
-    document.querySelector("#simbol2").innerHTML = result;
+    //document.querySelector("#simbol1").style.color = "red";
+    document.querySelector("#simbol1").innerHTML = result.paloSelect;
+    document.querySelector("#simbol1").style.color = result.color;
+    document.querySelector("#simbol2").innerHTML = result.paloSelect;
+    document.querySelector("#simbol2").style.color = result.color;
     document.querySelector("#number").innerHTML = generateNumber();
   });
 };
 
 function generateSimbol() {
-  let palo = ["&clubs;", "&spades;", "&hearts;", "&diams;"];
-  let paloIndx = Math.floor(Math.random() * palo.length);
-  return palo[paloIndx];
+  //   let pal1 = ["&clubs;", "&spades;"];
+  //   let pal2 = ["&hearts;", "&diams;"];
+  let palos = ["&clubs;", "&spades;", "&hearts;", "&diams;"];
+  let paloIndx = Math.floor(Math.random() * palos.length);
+  let paloColor;
+  let palo = palos[paloIndx];
+  if (palo === "&hearts;" || palo === "&diams;") {
+    paloColor = "red";
+  } else {
+    paloColor = "black";
+  }
+  return { paloSelect: palo, color: paloColor };
 }
 // //Generador de numeros aleatorios
 function generateNumber() {
